94b1c96e5dc2cd2e5ed9f7600ec2918aed69e697
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-04-10 22:03:30 +0100

    Create map displaying only shortest route

--- dijkstra.rb
+++ dijkstra.rb
@@ -11,3 +11,7 @@ distances = start.distances
 
 grid.distances = distances
 puts grid
+
+puts "Path from northwest corner to southeast corner"
+grid.distances = distances.path_to(grid[grid.rows - 1, grid.columns - 1])
+puts grid
