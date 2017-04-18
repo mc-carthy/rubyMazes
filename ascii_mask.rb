08446d03f7fda9d6ff85b462df2fe788b7b8f46a
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-04-18 11:11:59 +0100

    Define masks by text file

---
+++ ascii_mask.rb
@@ -0,0 +1,16 @@
+require 'mask'
+require 'masked_grid'
+
+require 'recursive_backtracker'
+
+abort "Please specify a text file to use as a template" if ARGV.empty?
+
+mask = Mask.from_txt(ARGV.first)
+grid = MaskedGrid.new(mask)
+
+RecursiveBacktracker.on(grid)
+
+filename = "mazeImages/maskedRecursiveBacktracker.png"
+grid.to_png.save(filename)
+
+puts "saved image to #{filename}"
