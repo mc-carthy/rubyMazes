b402b04ebff10651a3fe14d26db12d1568e55d09
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-04-19 19:18:28 +0100

    Weighted cells

---
+++ weighted_cell.rb
@@ -0,0 +1,32 @@
+require 'cell'
+
+class WeightedCell < Cell
+
+    attr_accessor :weight
+
+    def initialize(row, column)
+        super(row, column)
+        @weight = 1
+    end
+
+    def distances
+        weights = Distances.new(self)
+        pending = [self]
+
+        while pending.any?
+            cell = pending.sort_by { |c| weights[c] }.first
+            pending.delete(cell)
+
+            cell.links.each do |neighbour|
+                total_weight = weights[cell] + neighbour.weight
+
+                if !weights[neighbour] || total_weight < weights[neighbour]
+                    pending << neighbour
+                    weights[neighbour] = total_weight
+                end
+            end
+        end
+
+        weights
+    end
+end
