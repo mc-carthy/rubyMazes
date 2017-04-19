c63fdd68aec9c1049dfb18392dbad37e9b5d7eab
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-04-19 21:26:44 +0100

    Implement variable width when drawing mazes

--- recursive_backtracker_demo.rb
+++ recursive_backtracker_demo.rb
@@ -5,5 +5,5 @@ grid = Grid.new(20, 20)
 RecursiveBacktracker.on(grid)
 
 filename = "mazeImages/recursiveBacktracker.png"
-grid.to_png.save(filename)
+grid.to_png(10, 0.2).save(filename)
 puts "saved to #{filename}"
